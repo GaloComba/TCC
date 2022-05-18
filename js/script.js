82fa88a7e67c6f022cdcbc0c257635eb8b9cb3e9
Daniel Marelli <marelli_11@hotmail.com>
2022-05-18 02:18:57 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -115,3 +115,21 @@
     $window.trigger('scroll');
     });
    /*--------- End Timeline ---------*/
+
+   /*---------- Start Picture Galery --------*/
+   $(document).ready(function(){
+    $(".fancybox").fancybox({
+          openEffect: "none",
+          closeEffect: "none"
+      });
+      
+      $(".zoom").hover(function(){
+          
+          $(this).addClass('transition');
+      }, function(){
+          
+          $(this).removeClass('transition');
+      });
+  });
+
+  /*-------- End Picture Galery ----------*/
